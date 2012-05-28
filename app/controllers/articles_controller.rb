bbd2726f0dbafbf49d49acf0c09d03d0e5cd36a3
sighmon <simon.loffler@gmail.com>
2012-05-28 09:45:39 +0930

    Problem with the Article Controller routing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,8 @@
 class ArticlesController < ApplicationController
+	
+  	def create
+    	@issue = Issue.find(params[:issue_id])
+    	@article = @issue.article.create(params[:article])
+    	redirect_to issue_path(@issue)
+	end
 end
