ca9af7a06f86e80c5af73520f1613c2eefb4e2e1
sighmon <simon.loffler@gmail.com>
2012-06-06 16:14:14 +0930

    Added buy (purchase) functionality to articles on the Issue pages.

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -3,32 +3,34 @@ class PurchasesController < ApplicationController
     load_and_authorize_resource
 
     def new
-        # @issue = Issue.find(params[:issue_id])
+        @issue = Issue.find(params[:issue_id])
         @user = User.find(current_user)
         # TODO: How do we create a join, rather than a new issue?
-        @issue = @user.issues.build(params[:issue])
+        @purchase = @user.purchases.build(params[:issue])
 
         respond_to do |format|
             format.html # new.html.erb
             format.json { render json: @purchase }
         end
-  end
+    end
 
     def create
-        # @issue = Issue.find(params[:issue_id])
+        @issue = Issue.find(params[:issue_id])
         @user = User.find(current_user)
         # TODO: How do we create a join, rather than a new issue?
-        @issue = @user.issues.create(params[:issue])
+        # @purchase = @user.purchases.create(params[:issue])
+        # FIXME: Work out how to simplify this call.
+        @purchase = Purchase.create(:user_id => @user.id, :issue_id => @issue.id)
 
         respond_to do |format|
             if @purchase.save
                 format.html { redirect_to issue_path(@issue), notice: 'Issue was successfully purchased.' }
                 format.json { render json: @purchase, status: :created, location: @purchase }
             else
-                format.html { render action: "new" }
+                format.html { redirect_to issue_path(@issue), notice: "Couldn't purchase this issue." }
                 format.json { render json: @purchase.errors, status: :unprocessable_entity }
             end
         end
-  end
+    end
 
 end
