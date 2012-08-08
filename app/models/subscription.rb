413329385aa7254a79e3467590fcf9fcfab31cc5
sighmon <simon.loffler@gmail.com>
2012-08-08 16:23:03 +0930

    Recurring test

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,9 @@
 class Subscription < ActiveRecord::Base
   belongs_to :user
   attr_accessible :expiry_date, :user_id, :paypal_payer_id, :paypal_email, :paypal_profile_id, :paypal_first_name, :paypal_last_name, :refund
+
+  def recurring?
+  	return (not self.paypal_profile_id.nil?)
+  end
+
 end
