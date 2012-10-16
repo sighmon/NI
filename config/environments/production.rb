685ab8917ebe8ffbb53ba77b78f6c881a487f926
sighmon <simon.loffler@gmail.com>
2012-10-17 10:17:52 +1030

    Trying sending mail on port 25.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,7 @@ NI::Application.configure do
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
-    :port => 587,
+    :port => 25, #was 587
     :domain => "newint.herokuapp.com",
     :authentication => :plain,
     :enable_starttls_auto => true,
