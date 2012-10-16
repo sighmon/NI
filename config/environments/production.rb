12d8ea476ed752e875bb3d2a2c60ff4276c13230
sighmon <simon.loffler@gmail.com>
2012-10-17 10:08:09 +1030

    Show email send errors.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ NI::Application.configure do
   # Now using /config/application.yml figaro gem
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
