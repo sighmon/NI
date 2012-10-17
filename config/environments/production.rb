24d8a258e3c02d83d1c17386656f46d7ca0490ed
sighmon <simon.loffler@gmail.com>
2012-10-17 10:42:17 +1030

    Trying :domain as user_name.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,11 +11,10 @@ NI::Application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
-  # config.action_mailer.smtp_settings = {
-  ActionMailer::Base.smtp_settings = {
+  config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => 587,
-    # :domain => "gmail.com",
+    :domain => ENV["GMAIL_USER_NAME"],
     :authentication => :plain,
     :enable_starttls_auto => true,
     :user_name => ENV["GMAIL_USER_NAME"],
