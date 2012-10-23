8fa9c0b59092f0f8f23c5b5314a3bca6fde5b237
sighmon <simon.loffler@gmail.com>
2012-10-23 12:18:22 +1030

    Working on seeds.rb to seed Issues.

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,11 @@ Settings.issue_pagination = 12
 Settings.article_pagination = 10
 
 # TODO: setup the admin user
+
+# Setup an issue.
+
+Issue.new("issue"=>{"title"=>"Title", "number"=>"1234", "editors_name"=>"Editor Name", 
+	"editors_photo"=>File.open(Rails.root.join("public/uploads/issue/editors_photo/34/thumb_thumb_default_editors_photo.jpg")), 
+	"editors_letter"=>"Editor's Letter", 
+	"cover"=>File.open(Rails.root.join("public/uploads/issue/cover/34/thumb_thumb_default_cover.jpg")), 
+	"release(1i)"=>"2012", "release(2i)"=>"12", "release(3i)"=>"1", "release(4i)"=>"00", "release(5i)"=>"00", "trialissue"=>"0"})
