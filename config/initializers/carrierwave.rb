3fcd7c307214f54e2c19e74d01b6abb02652006d
sighmon <simon.loffler@gmail.com>
2012-10-23 11:53:25 +1030

    Fixed S3 configuration server name

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',
     :aws_access_key_id      => ENV['S3_KEY'],
     :aws_secret_access_key  => ENV['S3_SECRET'],
-    :region                 => 'tokyo'
+    :region                 => 'ap-northeast-1'
   }
   config.fog_directory  = 'nirailsapp'
   config.fog_public = true
