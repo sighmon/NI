4500da6678bb08d99dfe2b63b2851b5865377b20
pix <thatpixguy@gmail.com>
2012-10-24 23:02:40 +1030

    returned admin/settings#index test after working out how to login as admin first

--- spec/controllers/admin/settings_controller_spec.rb
+++ spec/controllers/admin/settings_controller_spec.rb
@@ -2,6 +2,13 @@ require 'spec_helper'
 
 describe Admin::SettingsController do
 
-
-
+  describe "GET 'index'" do
+    it "returns http success" do
+      user = FactoryGirl.create(:admin_user)
+      sign_in user
+      get 'index'
+      response.should be_success
+    end
+  end
+ 
 end
