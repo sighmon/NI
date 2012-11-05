9a36db628f90458e6bc6797d64585ad9f181159f
sighmon <simon.loffler@gmail.com>
2012-11-05 17:33:46 +1030

    Started work on encryption.

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -4,9 +4,15 @@ class Subscription < ActiveRecord::Base
 
   validates_presence_of :valid_from, :duration
 
- #  def recurring?
-	# return (not self.paypal_profile_id.nil?)
- #  end
+  # PayPal encryption
+  def encrypt_for_paypal(values)
+    signed = OpenSSL::PKCS7::sign(OpenSSL::X509::Certificate.new(ENV["APP_CERT_PEM"]),        OpenSSL::PKey::RSA.new(ENV["APP_KEY_PEM"], ''), values.map { |k, v| "#{k}=#{v}" }.join("\n"), [], OpenSSL::PKCS7::BINARY)
+    OpenSSL::PKCS7::encrypt([OpenSSL::X509::Certificate.new(ENV["PAYPAL_CERT_PEM"])], signed.to_der, OpenSSL::Cipher::Cipher::new("DES3"),        OpenSSL::PKCS7::BINARY).to_s.gsub("\n", "")
+  end
+
+  # def recurring?
+	#   return (not self.paypal_profile_id.nil?)
+  # end
 
   def is_current?
     return (expiry_date > DateTime.now and (not is_cancelled?))
