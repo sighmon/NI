adf0cc9286239507bc708e4707e09a285531831f
sighmon <simon.loffler@gmail.com>
2012-12-18 15:59:50 +1030

    Updated purchase page and subscription page to be a little more informative.

--- app/helpers/subscriptions_helper.rb
+++ app/helpers/subscriptions_helper.rb
@@ -1,27 +1,29 @@
 module SubscriptionsHelper	
 
     def subscription_price_table
-    	content_tag :table, :class => 'table table-bordered subscription-price-table' do
-    		content_tag :thead do
+    	content_tag :table, :class => 'table table-hover subscription-price-table' do
+    		content_tag(:thead,
     			content_tag(:tr,
-    				content_tag(:th, " ") +
+    				content_tag(:th, "Subscription options") +
     				content_tag(:th, "3 months") +
     				content_tag(:th, "6 months") +
     				content_tag(:th, "12 months")
-    			) +
-    			content_tag(:tr,
-    				content_tag(:td, "Once-up") +
-    				content_tag(:td, "$" + number_with_precision((Subscription.calculate_subscription_price(3,autodebit: false) / 100), :precision => 2)) +
-    				content_tag(:td, "$" + number_with_precision((Subscription.calculate_subscription_price(6,autodebit: false) / 100), :precision => 2)) +
-    				content_tag(:td, "$" + number_with_precision((Subscription.calculate_subscription_price(12,autodebit: false) / 100), :precision => 2))
-    			) +
-    			content_tag(:tr,
-    				content_tag(:td, "Ongoing Auto-debit") +
-    				content_tag(:td, "$" + number_with_precision((Subscription.calculate_subscription_price(3,autodebit: true) / 100), :precision => 2)) +
-    				content_tag(:td, "$" + number_with_precision((Subscription.calculate_subscription_price(6,autodebit: true) / 100), :precision => 2)) +
-    				content_tag(:td, "$" + number_with_precision((Subscription.calculate_subscription_price(12,autodebit: true) / 100), :precision => 2))
     			)
-    		end
+            ) +
+            content_tag(:tbody, 
+    			content_tag(:tr,
+    				content_tag(:td, "Single purchase") +
+    				content_tag(:td, "$" + cents_to_dollars(Subscription.calculate_subscription_price(3,autodebit: false))) +
+    				content_tag(:td, "$" + cents_to_dollars(Subscription.calculate_subscription_price(6,autodebit: false))) +
+    				content_tag(:td, "$" + cents_to_dollars(Subscription.calculate_subscription_price(12,autodebit: false)))
+    			) +
+    			content_tag(:tr,
+    				content_tag(:td, "Ongoing Automatic debit") +
+    				content_tag(:td, "$" + cents_to_dollars(Subscription.calculate_subscription_price(3,autodebit: true))) +
+    				content_tag(:td, "$" + cents_to_dollars(Subscription.calculate_subscription_price(6,autodebit: true))) +
+    				content_tag(:td, "$" + cents_to_dollars(Subscription.calculate_subscription_price(12,autodebit: true)))
+    			)
+    		)
     	end
     end
 
