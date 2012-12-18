823af0779e9033a97149ae867e37b49b1e096ea7
sighmon <simon.loffler@gmail.com>
2012-12-18 12:53:33 +1030

    New heroku bonsai settings.

--- config/initializers/bonsai.rb
+++ config/initializers/bonsai.rb
@@ -1,11 +1,20 @@
 # config/initializers/bonsai.rb
 
-if ENV['BONSAI_INDEX_URL']
-  Tire.configure do
-    url "http://index.bonsai.io/"
-  end
-  BONSAI_INDEX_NAME = ENV['BONSAI_INDEX_URL'][/[^\/]+$/]
-else
-  app_name = Rails.application.class.parent_name.underscore.dasherize
-  BONSAI_INDEX_NAME = "#{app_name}-#{Rails.env}"
-end
+ENV['ELASTICSEARCH_URL'] = ENV['BONSAI_URL']
+
+# Optional, but recommended: use a single index per application per environment
+app_name = Rails.application.class.parent_name.underscore.dasherize
+app_env = Rails.env
+BONSAI_INDEX_NAME = "#{app_name}-#{app_env}"
+
+
+
+# if ENV['BONSAI_INDEX_URL']
+#   Tire.configure do
+#     url "http://index.bonsai.io/"
+#   end
+#   BONSAI_INDEX_NAME = ENV['BONSAI_INDEX_URL'][/[^\/]+$/]
+# else
+#   app_name = Rails.application.class.parent_name.underscore.dasherize
+#   BONSAI_INDEX_NAME = "#{app_name}-#{Rails.env}"
+# end
