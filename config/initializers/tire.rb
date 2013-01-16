a797909afecfc6a790b6410542cdf3e2dd488404
pix <thatpixguy@gmail.com>
2013-01-16 13:46:12 +1030

    filter nill results from tire responses

--- config/initializers/tire.rb
+++ config/initializers/tire.rb
@@ -2,6 +2,7 @@ Tire::Results::Collection.class_eval do
     def __find_records_by_ids(klass, ids)
         #@options[:load] === true ? klass.find(ids) : klass.find(ids, @options[:load])
         # FIXME: not quite sure what the second (nil? false?) argument to klass.find would be doing...
-        klass.where(:id => ids)
+        #klass.where(:id => ids)
+        klass.where({:id => ids}).select{|a|!a.nil?}
     end
 end
