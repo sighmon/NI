06fe61f525b38dfc1a9135acd37039ebd6ccdf08
sighmon <simon.loffler@gmail.com>
2013-02-04 11:39:09 +1030

    Added redirect back to the page you were on before sign-in.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,22 @@ class ApplicationController < ActionController::Base
     redirect_to issues_path, :alert => exception.message
   end
 
+  # Save this page location to session
+  after_filter :store_location
+
   private
 
   def current_ability
     @current_ability ||= Ability.new(current_user, params[:utm_source])
+  end
+
+  def store_location
+    # store last url as long as it isn't a /users path
+    session[:previous_url] = request.fullpath unless request.fullpath =~ /\/users/
+  end
+
+  def after_sign_in_path_for(resource)
+    session[:previous_url] || root_path
   end
 
 end
