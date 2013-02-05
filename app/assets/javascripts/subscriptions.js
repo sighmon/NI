541f9c3237c4a9f1dcb31a58ae953ea331c0238d
sighmon <simon.loffler@gmail.com>
2013-02-05 13:37:39 +1030

    Added google analytics Goal code for Issue purchases and subscription purchases.

--- app/assets/javascripts/subscriptions.js
+++ app/assets/javascripts/subscriptions.js
@@ -1,6 +1,5 @@
-function trackConv(google_conversion_id,google_conversion_label,google_conversion_value) {
+function trackConv(google_conversion_id,google_conversion_label,google_conversion_value,purchase_type) {
     var image = new Image(1,1);
-    console.log("Before image.src");
     image.src = "http://www.googleadservices.com/pagead/conversion/"+google_conversion_id+"/?label="+google_conversion_label+"?value="+google_conversion_value+"&script=0";
-    console.log("After image.src");
+    _gaq.push(["_trackEvent",purchase_type,"purchase",google_conversion_label,google_conversion_value]);
 } 
