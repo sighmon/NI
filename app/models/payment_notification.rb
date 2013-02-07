a33bf83487c598920a86c8a279b4b5dc4d5ac337
sighmon <simon.loffler@gmail.com>
2013-02-08 10:24:16 +1030

    Ignore reversed payment IPN pings.

--- app/models/payment_notification.rb
+++ app/models/payment_notification.rb
@@ -17,6 +17,8 @@ private
 		elsif transaction_type == "cart"
 			# TODO: Implement handling instant payments.
 			logger.info "Instant purchase IPN ping received. TXN_ID: #{transaction_id}"
+		elsif payment_type == "instant"
+			logger.info "Instant purchase or refund IPN ping received. TXN_ID: #{transaction_id}"
 		elsif transaction_type == "recurring_payment_profile_created"
 			# TODO: Do we need to do anything with this?
 			logger.info "Recurring payment profile created: #{params[:recurring_payment_id]}"
