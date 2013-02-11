10f88174b77f356d185a2743bb40a6784c220f0b
sighmon <simon.loffler@gmail.com>
2013-02-11 14:12:54 +1030

    Modified adwords code to be in-page.

--- app/assets/javascripts/subscriptions.js
+++ app/assets/javascripts/subscriptions.js
@@ -1,5 +1,9 @@
-function trackConv(google_conversion_id,google_conversion_label,google_conversion_value,purchase_type) {
+// Code removed from 'complete your purchase' button
+// , :onclick => "trackConv(1072702417,'luYUCKztvAQQ0cfA_wM',#{number_with_precision((session[:express_purchase_price] / 100), :precision => 2)},'subscription');"
+
+// Not using it, embedded in page.
+/*function trackConv(google_conversion_id,google_conversion_label,google_conversion_value,purchase_type) {
     var image = new Image(1,1);
     image.src = "http://www.googleadservices.com/pagead/conversion/"+google_conversion_id+"/?label="+google_conversion_label+"?value="+google_conversion_value+"&script=0";
     _gaq.push(["_trackEvent",purchase_type,"purchase",google_conversion_label,google_conversion_value]);
-} 
+} */
