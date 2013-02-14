56bcb29d713f1b250e6a6c658affe5ea9697e2c9
sighmon <simon.loffler@gmail.com>
2013-02-14 16:42:42 +1030

    No default featured_image needed.

--- app/uploaders/featured_image_uploader.rb
+++ app/uploaders/featured_image_uploader.rb
@@ -21,12 +21,12 @@ class FeaturedImageUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  def default_url
-    # For Rails 3.1+ asset pipeline compatibility:
-    asset_path("fallback/" + [version_name, "default_featured_image.jpg"].compact.join('_'))
+  # def default_url
+  #   # For Rails 3.1+ asset pipeline compatibility:
+  #   asset_path("fallback/" + [version_name, "default_featured_image.jpg"].compact.join('_'))
   
-    #{ }"/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  end
+  #   #{ }"/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  # end
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
