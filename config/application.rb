4b081e21a9dc951dfc50de6ab80d492e9ede4f83
sighmon <simon.loffler@gmail.com>
2013-02-19 11:24:39 +1030

    Added images to assets path.

--- config/application.rb
+++ config/application.rb
@@ -65,5 +65,7 @@ module NI
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
     # add app/assets/html to the asset path
     config.assets.paths << "#{Rails.root}/app/assets/html"
+    config.assets.paths << Rails.root.join("app", "assets", "images")
+    config.assets.paths << Rails.root.join("app", "assets", "images", "fallback")
   end
 end
