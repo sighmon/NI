69cd7c6ae7c2410ba69ef9faa197066739ea8186
pix <thatpixguy@gmail.com>
2013-02-19 15:40:13 +1030

    spec fix

--- spec/controllers/admin/guest_passes_controller_spec.rb
+++ spec/controllers/admin/guest_passes_controller_spec.rb
@@ -3,9 +3,19 @@ require 'spec_helper'
 describe Admin::GuestPassesController do
 
   describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
-      response.should be_success
+    context "as a unauthorized user" do
+      it "redirects" do
+        get 'index'
+        response.should redirect_to(issues_url)
+      end
+    end
+
+    context "as an authorized user" do
+      it "returns http success" do
+        sign_in FactoryGirl.create(:admin_user)
+        get 'index'
+        response.should be_success
+      end
     end
   end
 
