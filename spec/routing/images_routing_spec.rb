1c22ff6459531dd0ef7f9fc0c694bec7deecae6c
pix <thatpixguy@gmail.com>
2013-02-23 22:41:30 +1030

    fixed image routing specs (not very dry)

--- spec/routing/images_routing_spec.rb
+++ spec/routing/images_routing_spec.rb
@@ -3,32 +3,36 @@ require "spec_helper"
 describe ImagesController do
   describe "routing" do
 
+    before (:each) do
+      image = FactoryGirl.create(:image)
+    end
+
     it "routes to #index" do
-      get("/images").should route_to("images#index")
+      get("/issues/1/articles/1/images").should route_to("images#index", :article_id => "1", :issue_id => "1")
     end
 
     it "routes to #new" do
-      get("/images/new").should route_to("images#new")
+      get("/issues/1/articles/1/images/new").should route_to("images#new", :article_id => "1", :issue_id => "1")
     end
 
     it "routes to #show" do
-      get("/images/1").should route_to("images#show", :id => "1")
+      get("/issues/1/articles/1/images/1").should route_to("images#show", :id => "1", :article_id => "1", :issue_id => "1")
     end
 
     it "routes to #edit" do
-      get("/images/1/edit").should route_to("images#edit", :id => "1")
+      get("/issues/1/articles/1/images/1/edit").should route_to("images#edit", :id => "1", :article_id => "1", :issue_id => "1")
     end
 
     it "routes to #create" do
-      post("/images").should route_to("images#create")
+      post("/issues/1/articles/1/images").should route_to("images#create", :article_id => "1", :issue_id => "1")
     end
 
     it "routes to #update" do
-      put("/images/1").should route_to("images#update", :id => "1")
+      put("/issues/1/articles/1/images/1").should route_to("images#update", :id => "1", :article_id => "1", :issue_id => "1")
     end
 
     it "routes to #destroy" do
-      delete("/images/1").should route_to("images#destroy", :id => "1")
+      delete("/issues/1/articles/1/images/1").should route_to("images#destroy", :id => "1", :article_id => "1", :issue_id => "1")
     end
 
   end
