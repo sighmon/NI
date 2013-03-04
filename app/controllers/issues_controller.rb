c1a5e7fbe255b29edbdadb6667c989d65f6fd7b1
sighmon <simon.loffler@gmail.com>
2013-03-04 14:30:56 +1030

    Moved mixed media to their own reviews section.

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -72,9 +72,10 @@ class IssuesController < ApplicationController
     @agendas = @issue.articles_of_category("/sections/agenda/").sort_by(&:publication)
     @arguments = @issue.articles_of_category("/argument/").sort_by(&:publication)
     @alternatives = @issue.articles_of_category("/alternatives/").sort_by(&:publication)
-    @columns = @issue.articles_of_category("/columns/").sort_by(&:publication)
+    @regulars = (@issue.articles_of_category("/columns/") - @issue.articles_of_category("/columns/media/")).sort_by(&:publication)
+    @mixedmedia = @issue.articles_of_category("/columns/media/").sort_by(&:publication)
     @blogs = @issue.articles_of_category("/blog/").sort_by(&:publication)
-    @uncategorised = @all_articles - [@keynote] - @features - @agendas - @arguments - @columns - @blogs - @alternatives
+    @uncategorised = @all_articles - [@keynote] - @features - @agendas - @arguments - @regulars - @blogs - @alternatives - @mixedmedia
 
     # Set meta tags
     set_meta_tags :title => @issue.title,
