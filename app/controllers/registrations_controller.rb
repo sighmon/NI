221b31260915f61df40b9b5b6cf5ac47b14d5b02
sighmon <simon.loffler@gmail.com>
2013-03-14 20:28:21 +1030

    Removed before filter, else non-registered users can't register.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
 	# Cancan authorisation
 	# load_and_authorize_resource
-	before_filter :can_update
+	# TOFIX: before_filter :can_update
 
 	private
 
