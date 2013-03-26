4d8a858ac4224c9a8e5284e19d985d5de115b34c
sighmon <simon.loffler@gmail.com>
2013-03-26 16:39:22 +1030

    Added default_article_image.jpg and updated the fallback code.

--- app/uploaders/article_image_uploader.rb
+++ app/uploaders/article_image_uploader.rb
@@ -24,7 +24,7 @@ class ArticleImageUploader < CarrierWave::Uploader::Base
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
     # For Rails 3.1+ asset pipeline compatibility:
-    asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+    asset_path("fallback/" + [version_name, "default_article_image.jpg"].compact.join('_'))
   
     # "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   end
