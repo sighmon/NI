9994f81c53db90278840e2d8662b5ba07aca585e
sighmon <simon.loffler@gmail.com>
2013-08-12 12:11:35 +0930

    Added a redirect from sitemap.xml to S3.

---
+++ app/controllers/sitemap_controller.rb
@@ -0,0 +1,9 @@
+class SitemapController < ApplicationController
+	def index
+		redirect_to sitemap_url
+	end
+end
+
+def sitemap_url
+	"http://#{CarrierWave::Uploader::Base.fog_directory}.s3.amazonaws.com/sitemaps/sitemap_index.xml.gz"
+end
