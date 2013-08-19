f2d122eb434ebf8f6e7cb95e34f17f3d277a9a34
sighmon <simon.loffler@gmail.com>
2013-08-19 14:46:21 +0930

    Added open_graph and twitter card meta tags to the home page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,5 +5,30 @@ class HomeController < ApplicationController
   	else
   		@issues = Issue.find_all_by_published(:true)
   	end
+
+  	# Set meta tags
+    @page_title = "New Internationalist Magazine Digital Edition"
+    @page_description = "Welcome to the digital edition of the New Internationalist magazine, available for all digital devices with a browser."
+
+    set_meta_tags :title => @page_title,
+                  :description => @page_description,
+                  :keywords => "new, internationalist, magazine, archive, digital, edition, australia",
+                  :open_graph => {
+                    :title => @page_title,
+                    :description => @page_description,
+                    :url   => root_url,
+                    :image => @issues.sort_by{|i| i.release}.last.try(:cover_url, :thumb2x).to_s,
+                    :site_name => "New Internationalist Magazine Digital Edition"
+                  },
+                  :twitter => {
+                    :card => "summary",
+                    :site => "@ni_australia",
+                    :creator => "@sighmon",
+                    :title => @page_title,
+                    :description => @page_description,
+                    :image => {
+                      :src => @issues.sort_by{|i| i.release}.last.try(:cover_url, :thumb2x).to_s
+                    }
+                  }
   end
 end
