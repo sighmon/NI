dd5fc46d8d5a31c409da9f18e4315330859bf450
sighmon <simon.loffler@gmail.com>
2013-09-17 10:02:34 +0930

    Using .ruby-gemset and .ruby-version instead of .rvmrc

--- app/controllers/payment_notifications_controller.rb
+++ app/controllers/payment_notifications_controller.rb
@@ -1,18 +1,18 @@
 class PaymentNotificationsController < ApplicationController
-  
-  protect_from_forgery :except => [:create]
+	
+	protect_from_forgery :except => [:create]
 
-  def create
-    # logger.info params
-    
-    # TOFIX: Hack to handle 'cart' instant payment notifications.
-    if params[:txn_type] == "cart"
-      params[:rp_invoice_id] == "1"
-    end
+	def create
+		# logger.info params
+		
+		# TOFIX: Hack to handle 'cart' instant payment notifications.
+		if params[:txn_type] == "cart"
+			params[:rp_invoice_id] == "1"
+		end
 
-    #using activemerchant to validate the IPN: https://go.developer.ebay.com/developers/community/blogs/saranyan/paypal-ipn-using-rails-3.1-and-active-merchant
+		# TODO: using activemerchant to validate the IPN: https://go.developer.ebay.com/developers/community/blogs/saranyan/paypal-ipn-using-rails-3.1-and-active-merchant
 
-    PaymentNotification.create!(:params => params, :user_id => params[:rp_invoice_id], :status => params[:payment_status], :transaction_id => params[:txn_id], :transaction_type => params[:txn_type] )
-      render :nothing => true
-  end
+		PaymentNotification.create!(:params => params, :user_id => params[:rp_invoice_id], :status => params[:payment_status], :transaction_id => params[:txn_id], :transaction_type => params[:txn_type] )
+		render :nothing => true
+	end
 end
