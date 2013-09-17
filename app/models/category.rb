9000cea3a3bf32147c3bb2570f1a05222849abdd
sighmon <simon.loffler@gmail.com>
2013-09-17 17:29:52 +0930

    Added hsv_to_rgb helper to Category.

--- app/models/category.rb
+++ app/models/category.rb
@@ -48,4 +48,19 @@ class Category < ActiveRecord::Base
     end
   end
 
+  def self.hsv_to_rgb(h, s, v)
+    h_i = (h*6).to_i
+    f = h*6 - h_i
+    p = v * (1 - s)
+    q = v * (1 - f*s)
+    t = v * (1 - (1 - f) * s)
+    r, g, b = v, t, p if h_i==0
+    r, g, b = q, v, p if h_i==1
+    r, g, b = p, v, t if h_i==2
+    r, g, b = p, q, v if h_i==3
+    r, g, b = t, p, v if h_i==4
+    r, g, b = v, p, q if h_i==5
+    [(r*255).to_i, (g*255).to_i, (b*255).to_i]
+  end
+
 end
