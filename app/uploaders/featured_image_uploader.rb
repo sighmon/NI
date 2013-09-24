d283a7dc6e48335cd4f85d522675acdb43ba3dc7
sighmon <simon.loffler@gmail.com>
2013-09-24 10:55:02 +0930

    Removing default_url from featured_image_uploader.rb so that we can test against null for articles that don't have a featured image.

--- app/uploaders/featured_image_uploader.rb
+++ app/uploaders/featured_image_uploader.rb
@@ -21,12 +21,12 @@ class FeaturedImageUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  def default_url
-    # For Rails 3.1+ asset pipeline compatibility:
-    asset_path("fallback/" + full_filename("default_featured_image.jpg"))
+  # def default_url
+  #   # For Rails 3.1+ asset pipeline compatibility:
+  #   asset_path("fallback/" + full_filename("default_featured_image.jpg"))
   
-    #{ }"/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  end
+  #   #{ }"/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  # end
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
