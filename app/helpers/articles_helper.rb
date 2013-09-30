d9ff852c8789e7ec210bca30c0ef7e21b245b8d6
sighmon <simon.loffler@gmail.com>
2013-09-30 17:21:38 +0930

    Fixed bug for full images in articles on retina devices.

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -198,7 +198,11 @@ module ArticlesHelper
           caption_div = "<div class='new-image-caption'>#{image.caption}</div>"
         end
         if media_url
-          "<div class='#{css_class}'>"+retina_image_tag(media_url, :alt => "#{strip_tags(image.caption)}", :title => "#{strip_tags(image.caption)}", :size => "#{image_width}x#{image_width * image.height / image.width}")+caption_div+credit_div+"</div>"
+          if options.include?("full")
+            "<div class='#{css_class}'>"+image_tag(media_url, :alt => "#{strip_tags(image.caption)}", :title => "#{strip_tags(image.caption)}", :size => "#{image_width}x#{image_width * image.height / image.width}")+caption_div+credit_div+"</div>"
+          else
+            "<div class='#{css_class}'>"+retina_image_tag(media_url, :alt => "#{strip_tags(image.caption)}", :title => "#{strip_tags(image.caption)}", :size => "#{image_width}x#{image_width * image.height / image.width}")+caption_div+credit_div+"</div>"
+          end
         else
           ""
         end
