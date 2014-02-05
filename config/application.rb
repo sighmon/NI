d45defb240c8fe1ebb7958734a3497c5d75cc141
sighmon <simon.loffler@gmail.com>
2014-02-05 18:23:11 -0500

    Added config.i18n.enforce_available_locales = true to application.rb to make Rails happy.

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,9 @@ module NI
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    # To supress deprecation warning for Rails 3.2.14 and above
+    config.i18n.enforce_available_locales = true
+
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
