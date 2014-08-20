12421330e2a7f61d66bf42ca73ca5abdf56e830d
sighmon <simon.loffler@gmail.com>
2014-08-20 15:53:23 -0400

    Added try incase the category article isn't published yet, or doesn't have an image.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -41,7 +41,7 @@ class CategoriesController < ApplicationController
     # Set meta tags
     @page_title = "Articles about #{@category.short_display_name}"
     @page_description = "All New Internationalist articles about #{@category.short_display_name}, ordered by date."
-    @category_image = @category.latest_published_article.first_image.try(:data).to_s
+    @category_image = @category.try(:latest_published_article).try(:first_image).try(:data).to_s
 
     set_meta_tags :title => @page_title,
                   :description => @page_description,
