b5ba4f2cab39bf956f09f3555c09deef211217ef
pix <thatpixguy@gmail.com>
2015-07-10 16:24:49 +0930

    add permitted parameters for registrations controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,13 +1,22 @@
 class RegistrationsController < Devise::RegistrationsController
-	# Cancan authorisation
-	# load_and_authorize_resource
-	
-	# TOFIX: Allow users to register, but authorize all users with a parent.
-	before_filter :can_update, :only => [:edit, :update]
+  # Cancan authorisation
+  # load_and_authorize_resource
 
-	private
+  # TOFIX: Allow users to register, but authorize all users with a parent.
+  before_filter :can_update, :only => [:edit, :update]
 
-	def can_update
-		authorize! :update, current_user
-	end
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :current_password) }
+  end
+
+  private
+
+  def can_update
+    authorize! :update, current_user
+  end
 end
