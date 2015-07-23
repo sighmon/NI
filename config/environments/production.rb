c5306c35254cd512779d3b797cd636617930ff03
sighmon <simon.loffler@gmail.com>
2015-07-23 14:19:25 +0930

    Updated syntax to config.serve_static_files for Rails 4.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,7 +97,7 @@ NI::Application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   # config.serve_static_assets = false
-  config.serve_static_assets = true # for memcached
+  config.serve_static_files = true # for memcached
 
   # Compress JavaScripts and CSS
   # config.assets.compress = true
