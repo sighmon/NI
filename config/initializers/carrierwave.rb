b6c45b79603e59f4fc19b9ad53a08d3ce041ced7
sighmon <simon.loffler@gmail.com>
2015-08-06 13:18:41 +0930

    Use CloudFront CDN for Carrierwave asset serving which is used by the iOS and Android apps via the issues.json feed.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,6 +6,8 @@ CarrierWave.configure do |config|
     :region                 => ENV['S3_REGION']
   }
   config.fog_directory  = ENV['S3_BUCKET']
+  # Use CloudFront CDN
+  config.asset_host = "https://#{ENV['CLOUDFRONT_SERVER']}.cloudfront.net"
   config.fog_public = true
   config.fog_attributes = {'Cache-Control'=>'max-age=31557600'}
   # config.fog_host = 's3-ap-northeast-1.amazonaws.com'
