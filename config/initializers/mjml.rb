64f666410ab327d9a87548c52b1f09d5bd9429b8
sighmon <simon.loffler@gmail.com>
2016-05-06 16:46:50 +0930

    Added new html layout for the UK sign-up confirmation email.

--- config/initializers/mjml.rb
+++ config/initializers/mjml.rb
@@ -2,6 +2,7 @@ class Mjml::Rails::ERBContext
   include ActionView::Helpers
   include ::Rails.application.routes.url_helpers
   default_url_options[:host] = Rails.env.production? ? ENV['NI_APP_HOST'] : 'localhost:3000'
-  asset_host = Rails.env.production? ? ENV['NI_APP_HOST'] : 'localhost:3000'
+  # TODO: Fix setting this correctly. Use Cloudfront.
+  # asset_host = Rails.env.production? ? ENV['NI_APP_HOST'] : 'localhost:3000'
 end
 
