7e96a1f401b0d5145227f012ab9b5271a5bc01a4
sighmon <simon.loffler@gmail.com>
2016-05-06 10:50:18 +0930

    Adding asset_host to the mjml initialiser.

--- config/initializers/mjml.rb
+++ config/initializers/mjml.rb
@@ -2,5 +2,6 @@ class Mjml::Rails::ERBContext
   include ActionView::Helpers
   include ::Rails.application.routes.url_helpers
   default_url_options[:host] = Rails.env.production? ? ENV['NI_APP_HOST'] : 'localhost:3000'
+  asset_host = Rails.env.production? ? ENV['NI_APP_HOST'] : 'localhost:3000'
 end
 
