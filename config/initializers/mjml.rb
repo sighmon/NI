cef165a6085f07af434b76cf4efffa60b755421d
sighmon <simon.loffler@gmail.com>
2016-10-25 14:05:47 +1030

    Updated mjml-rails to 2.4.0 which allows for other template languages. We're still using :erb

--- config/initializers/mjml.rb
+++ config/initializers/mjml.rb
@@ -9,3 +9,6 @@
 #   # asset_host = Rails.env.production? ? ENV['NI_APP_HOST'] : 'localhost:3000'
 # end
 
+# Mjml.setup do |config|
+#   config.template_language = :erb # :erb (default), :slim, :haml, or any other you are using
+# end
