05c9ac31631f06232ece136b51448028bb509fcf
sighmon <simon.loffler@gmail.com>
2017-09-08 15:17:36 +0930

    Updated rspec syntax.

--- spec/controllers/institution/users_controller_spec.rb
+++ spec/controllers/institution/users_controller_spec.rb
@@ -119,7 +119,10 @@ describe Institution::UsersController, :type => :controller do
           # receives the :update_attributes message with whatever params are
           # submitted in the request.
           child_user_params = FactoryGirl.attributes_for(:child_user)
-          expect_any_instance_of(User).to receive(:update_attributes).with(child_user_params)
+          # TOFIX: Ugly hack merging uk_id & uk_expiry to "" insetad of nil
+          child_user_action_params = ActionController::Parameters.new(child_user_params.merge({uk_id: "",uk_expiry: ""})).permit(:username, :email, :uk_id, :uk_expiry, :password, :password_confirmation)
+          # byebug
+          expect_any_instance_of(User).to receive(:update_attributes).with(child_user_action_params)
           put :update, params: {:id => user.to_param, :user => child_user_params}
         end
 
