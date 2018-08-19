771a2e24c9ef5c565577bd0dd8f51e903ac49963
sighmon <simon.loffler@gmail.com>
2018-08-19 15:26:18 +1000

    Another attempt to get it running on Staging.

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -21,7 +21,7 @@ SecureHeaders::Configuration.default do |config|
     # directive values: these values will directly translate into source directives
     # default_src: %w(https: 'self'),
     default_src: %W(https: 'self' #{URI(ENV['NI_APP_HOST']).host}),
-    base_uri: %w('self'),
+    base_uri: %W('self' #{URI(ENV['NI_APP_HOST']).host}),
     block_all_mixed_content: Rails.env.production?, # see http://www.w3.org/TR/mixed-content/
     child_src: %w('self' *.facebook.com *.facebook.net *.twitter.com *.disqus.com disqus.com *.youtube.com *.googletagmanager.com public.tableau.com uploads.knightlab.com player.vimeo.com *.google.com), # if child-src isn't supported, the value for frame-src will be set.
     connect_src: %w('self' wss: *.google-analytics.com *.disqus.com),
