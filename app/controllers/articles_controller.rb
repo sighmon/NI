04f56151760794578da0a7772479db29f7c36520
sighmon <simon.loffler@gmail.com>
2018-09-03 23:33:41 +1000

    Added Agony Uncle, Hall of Infamy, View from India, View from America and new mixed-media styling.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -290,6 +290,8 @@ class ArticlesController < ApplicationController
     @newimage = Image.new
     @letters = @article.categories.select{|c| c.name.include?("/letters/")}
     @view_from_africa = @article.categories.select{|c| c.name.include?("/columns/view-from-africa/")}
+    @view_from_india = @article.categories.select{|c| c.name.include?("/columns/view-from-india/")}
+    @view_from_america = @article.categories.select{|c| c.name.include?("/columns/view-from-america/")}
     @debate = @article.categories.select{|c| c.name.include?("/sections/argument/")}
     @temperature_check = @article.categories.select{|c| c.name.include?("/columns/temperature-check/")}
     # Push the single top image to the right for these categories
