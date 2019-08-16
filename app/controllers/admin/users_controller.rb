42f286476eedec6cf932c921cf44283ae5b1093f
sighmon <simon.loffler@gmail.com>
2019-08-16 14:19:48 +1000

    Allow search on full name in admin.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -331,7 +331,7 @@ class Admin::UsersController < Admin::BaseController
 		if @query.blank?
 			@users = []
 		else
-			@users = User.where('email ~* :query or username ~* :query or first_name ~* :query or last_name ~* :query or company_name ~* :query or address ~* :query or comments ~* :query', { query: @query }).sorted_by(params[:sort], params[:direction])
+			@users = User.where("email ~* :query or username ~* :query or first_name ~* :query or last_name ~* :query or CONCAT(first_name,' ', last_name) ~* :query or company_name ~* :query or address ~* :query or comments ~* :query", { query: @query }).sorted_by(params[:sort], params[:direction])
 		end
 		respond_to do |format|
 			format.html
