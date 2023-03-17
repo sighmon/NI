0ec02efdc1ffd48364462310c8dc27ec36ed29d7
sighmon <simon.loffler@gmail.com>
2023-03-17 15:30:50 +1030

    Remove JQuery 1 and precompile assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
+//= require jquery3
 //= require jquery-ui
-//= require jquery_ujs
+//= require rails-ujs
 //= require jquery-fileupload/basic
 //= require jquery-fileupload/vendor/tmpl
 
