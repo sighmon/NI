a17313f85b5eb725bcacea057e2e47ddecace760
sighmon <simon.loffler@gmail.com>
2024-01-19 14:32:11 +1030

    Add is_recurring to UK export to show automatic renewing subscriptions

--- app/models/user.rb
+++ app/models/user.rb
@@ -222,6 +222,7 @@ class User < ActiveRecord::Base
     id? 'download_permissions' do '' end
     uk_user? 'uk_account'
     user_type 'user_type'
+    is_recurring? 'is_recurring'
     parent_id 'parent_id'
 
   end
