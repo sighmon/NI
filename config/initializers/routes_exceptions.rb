94eb81d742620b53f7515c9896d94814aab83cfc
sighmon <simon.loffler@gmail.com>
2024-06-21 10:07:56 +0930

    Add .php7 to 403 routes

--- config/initializers/routes_exceptions.rb
+++ config/initializers/routes_exceptions.rb
@@ -1,2 +1,2 @@
 # Ignore any requests to Wordpress login pages.
-RESPOND_403 = ['wp-login.php', 'xmlrpc.php', '(*url).php']
+RESPOND_403 = ['wp-login.php', 'xmlrpc.php', '(*url).php', '(*url).php7', '(*url).PhP7']
